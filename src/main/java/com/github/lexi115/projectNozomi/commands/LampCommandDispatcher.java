127ab9986f6fbfc20dde4c6e473075a38c0cb61e
Lexi115 <shinokataku1001@gmail.com>
2025-11-28 17:54:32 +0100

    Additional checks when an exception is thrown from a command method
    Signed-off-by: Lexi115 <shinokataku1001@gmail.com>

--- src/main/java/com/github/lexi115/projectNozomi/commands/LampCommandDispatcher.java
+++ src/main/java/com/github/lexi115/projectNozomi/commands/LampCommandDispatcher.java
@@ -7,14 +7,16 @@ import lombok.NonNull;
 import revxrsal.commands.Lamp;
 import revxrsal.commands.bukkit.BukkitLamp;
 import revxrsal.commands.bukkit.actor.BukkitCommandActor;
+import revxrsal.commands.bukkit.exception.InvalidPlayerException;
+import revxrsal.commands.bukkit.exception.SenderNotPlayerException;
 import revxrsal.commands.command.Potential;
+import revxrsal.commands.exception.InvalidIntegerException;
 import revxrsal.commands.exception.NoPermissionException;
 import revxrsal.commands.exception.UnknownCommandException;
 import revxrsal.commands.node.DispatcherSettings;
 import revxrsal.commands.stream.StringStream;
 
 import java.util.List;
-import java.util.Objects;
 
 @Singleton
 public class LampCommandDispatcher implements CommandDispatcher {
@@ -60,12 +62,35 @@ public class LampCommandDispatcher implements CommandDispatcher {
             final StringStream input
     ) {
         for (var attempt : failedAttempts) {
-            if (attempt.error() instanceof NoPermissionException) {
-                var error = Objects.requireNonNull((NoPermissionException) attempt.error());
-                exceptionHandler.onNoPermission(error, sender);
+            var error = attempt.error();
+            if (error instanceof NoPermissionException) {
+                handleException(NoPermissionException.class, error, sender, exceptionHandler::onNoPermission);
+                return;
+            } else if (error instanceof InvalidPlayerException) {
+                handleException(InvalidPlayerException.class, error, sender, exceptionHandler::onInvalidPlayer);
+                return;
+            } else if (error instanceof InvalidIntegerException) {
+                handleException(InvalidIntegerException.class, error, sender, exceptionHandler::onInvalidInteger);
+                return;
+            } else if (error instanceof SenderNotPlayerException) {
+                handleException(SenderNotPlayerException.class, error, sender, exceptionHandler::onSenderNotPlayer);
                 return;
             }
         }
         exceptionHandler.onUnknownCommand(new UnknownCommandException(input.toString()), sender);
+    }
+
+    private <T extends Throwable> void handleException(
+            final @NonNull Class<T> clazz,
+            final Throwable e,
+            final BukkitCommandActor actor,
+            final HandlerMethod<T> method) {
+        if (clazz.isInstance(e)) {
+            method.handle(clazz.cast(e), actor);
+        }
+    }
+
+    interface HandlerMethod<T extends Throwable> {
+        void handle(T e, BukkitCommandActor actor);
     }
 }
